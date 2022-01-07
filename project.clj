3120d17bb3f14e06a5fecf4edc04bf9ae42cfec4
Sebastian Poeplau <poeplau@code-intelligence.com>
2022-01-07 15:53:37 +0100

    Use version 0.1.0 of jazzer-clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.code-intelligence/jazzer-clj "0.1.0-SNAPSHOT"]
+                 [com.code-intelligence/jazzer-clj "0.1.0"]
                  [metosin/jsonista "0.3.5"]]
   :repl-options {:init-ns jazzer-clojure.core}
   :profiles {:uberjar {:aot :all
